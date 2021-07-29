c921490076d27825519913fe8d041c36c5c8f976
Hunter Oppel <hunter.oppel@gmail.com>
2021-07-29 05:27:34 -0400

    Setting grid size for game
    Currently not working

---
+++ Homer Memory Game/Homer Memory Game/Cells/CardCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CardCollectionViewCell.swift
+//  Homer Memory Game
+//
+//  Created by Hunter Oppel on 7/29/21.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Homer Memory Game/Homer Memory Game/Model/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Homer Memory Game
+//
+//  Created by Hunter Oppel on 7/29/21.
+//
+
+import Foundation
