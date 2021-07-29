4354641a9a745b24caf7643d055865f367157aa1
Hunter Oppel <hunter.oppel@gmail.com>
2021-07-29 04:32:44 -0400

    Simple Navigation

---
+++ Homer Memory Game/Homer Memory Game/View/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Homer Memory Game
+//
+//  Created by Hunter Oppel on 7/29/21.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
